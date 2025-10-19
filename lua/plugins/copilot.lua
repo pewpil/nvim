30243a43e86e9d67f18fc3ee2328ec1762f2d92e
pewpil <pldnllrnt1012@gmail.com>
2025-10-19 13:24:01 +0800

    other options added

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,8 @@
 return {
-	"github/copilot.vim"
+	"github/copilot.vim",
+	config = function ()
+		local opts = { noremap = true, silent = true, desc = "Enable Copilot" }
+		vim.keymap.set("n", "<leader>Ce", ":Copilot enable<CR>", opts)
+		vim.keymap.set("n", "<leader>Cd", ":Copilot disable<CR>", opts)
+	end
 }
--- lua/plugins/live-preview.lua
+++ lua/plugins/live-preview.lua
@@ -1,3 +1,7 @@
 return {
 	"brianhuster/live-preview.nvim",
+	config = function ()
+		vim.keymap.set("n", "<leader>lps", ":LivePreview start<CR>", {silent = true})
+		vim.keymap.set("n", "<leader>lpS", ":LivePreview stop<CR>", {silent = true})
+	end
 }
